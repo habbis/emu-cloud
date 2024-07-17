d91901d3a0ca89ff7f305726ce5f022bef033645
habbis <habbis@habbestad.org>
2024-07-17 22:53:35 +0000

    created a fuction for proxmox api request in vm program

--- vm/cmd/main.go
+++ vm/cmd/main.go
@@ -6,11 +6,11 @@ import (
 	"github.com/go-resty/resty/v2"
 	"log"
 	"os"
-	"strings"
 )
 
 type Json_config struct {
 	Pve_host     string
+	Pve_node     string
 	Pve_api_user string
 	Pve_api_key  string
 	Pve_template int
@@ -24,51 +24,62 @@ type Clone_vm struct {
 	Storage string `json:"storage"`
 }
 
-func Send_request() {
-
-	var config = ".config.json"
-
-	content, err := os.ReadFile(config)
-	if err != nil {
-		log.Fatal("Error when opening file: ", err)
-	}
-
-	var json_payload Json_config
-	err = json.Unmarshal(content, &json_payload)
-
-	// using split to get pve node from url
-	pve_host := json_payload.Pve_host
-	split_res1 := strings.Split(pve_host, ".")
-	res2 := split_res1[0]
-	split_res2 := strings.Split(res2, ":")
-	res3 := split_res2[1]
-	split_res3 := strings.Split(res3, "//")
-	pve_node := split_res3[1]
-
-	pve_api_clone := fmt.Sprintf("%s/api2/json/nodes/%s/qemu/%d/clone", json_payload.Pve_host, pve_node, json_payload.Pve_template)
-	pve_api_creds := fmt.Sprintf("PVEAPIToken=%s=%s", json_payload.Pve_api_user, json_payload.Pve_api_key)
-
-	data := Clone_vm{
-		Newid:   127,
-		Node:    pve_node,
-		Name:    "hf-test2",
-		Full:    true,
-		Storage: "local-storage",
-	}
+func Send_pve_request(http_method string, api_call string, pve_api_creds string, data interface{}) string {
 
 	client := resty.New()
+	if http_method == "GET" {
+		response, err := client.R().
+			EnableTrace().
+			SetHeader("Accept", "application/json").
+			SetHeader("Authorization", pve_api_creds).
+			Get(api_call)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return response.Status()
 
-	response, err := client.R().
-		EnableTrace().
-		SetHeader("Accept", "application/json").
-		SetHeader("Authorization", pve_api_creds).
-		SetBody(&data).
-		Post(pve_api_clone)
-	if err != nil {
-		log.Fatal(err)
+	} else if http_method == "POST" {
+		response, err := client.R().
+			EnableTrace().
+			SetHeader("Accept", "application/json").
+			SetHeader("Authorization", pve_api_creds).
+			SetBody(data).
+			Post(api_call)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return response.Status()
+
+	} else if http_method == "PUT" {
+		response, err := client.R().
+			EnableTrace().
+			SetHeader("Accept", "application/json").
+			SetHeader("Authorization", pve_api_creds).
+			SetBody(data).
+			Put(api_call)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("PUT Response:", response.Status())
+		return response.Status()
+
+	} else if http_method == "DELETE" {
+		response, err := client.R().
+			EnableTrace().
+			SetHeader("Accept", "application/json").
+			SetHeader("Authorization", pve_api_creds).
+			SetBody(data).
+			Post(api_call)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return response.Status()
+	} else {
+		wrong := "Wrong HTTP Method given!"
+		return wrong
+
 	}
-	fmt.Println("POST Response:", response.Status())
-
+	return ""
 }
 
 func main() {
@@ -83,17 +94,7 @@ func main() {
 	var json_payload Json_config
 	err = json.Unmarshal(content, &json_payload)
 
-	// using split to get pve node from url
-	pve_host := json_payload.Pve_host
-	split_res1 := strings.Split(pve_host, ".")
-	res2 := split_res1[0]
-	split_res2 := strings.Split(res2, ":")
-	res3 := split_res2[1]
-	split_res3 := strings.Split(res3, "//")
-	pve_node := split_res3[1]
-
-	pve_api_clone := fmt.Sprintf("%s/api2/json/nodes/%s/qemu/%d/clone", json_payload.Pve_host, pve_node, json_payload.Pve_template)
-	pve_api_creds := fmt.Sprintf("PVEAPIToken=%s=%s", json_payload.Pve_api_user, json_payload.Pve_api_key)
+	pve_node := json_payload.Pve_node
 
 	data := Clone_vm{
 		Newid:   127,
@@ -103,17 +104,10 @@ func main() {
 		Storage: "local-storage",
 	}
 
-	client := resty.New()
+	pve_api_clone := fmt.Sprintf("%s/api2/json/nodes/%s/qemu/%d/clone", json_payload.Pve_host, json_payload.Pve_node, json_payload.Pve_template)
+	pve_api_creds := fmt.Sprintf("PVEAPIToken=%s=%s", json_payload.Pve_api_user, json_payload.Pve_api_key)
 
-	response, err := client.R().
-		EnableTrace().
-		SetHeader("Accept", "application/json").
-		SetHeader("Authorization", pve_api_creds).
-		SetBody(&data).
-		Post(pve_api_clone)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("POST Response:", response.Status())
+	clone_vm := Send_pve_request("POST", pve_api_clone, pve_api_creds, data)
+	fmt.Println(clone_vm)
 
 }
